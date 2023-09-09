38575600b771248c66683508249da4120d271a24
Iqyan Aziz Syamaidzar <azizsyam503@gmail.com>
2023-09-09 15:21:55 +0700

    firstCommit

---
+++ fungsi.js
@@ -0,0 +1,16 @@
+const add = (a, b) => a + b;
+console.log(add(1,2));
+
+const substrack = (a, b) => a - b;
+console.log(substrack(5,2));
+
+const multiplyNumber = (a, b) => a * b;
+console.log(multiplyNumber(2, 2));
+
+const divide = (a, b) => a / b;
+console.log(divide(4, 2));
+
+console.log(add(5, 5));
+
+const mathSin = (number) => Math.sin(number);
+console.log(mathSin(10));
